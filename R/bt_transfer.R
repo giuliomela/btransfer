3997f193d83f9ceb2ec52fc52bc59b81f8676aa8
giulio.mela <giulio.mela@gmail.com>
2022-04-06 18:12:50 +0200

    Creato cript per creazione database e finalizzato script per beneft transfer (senza chiamate alla API della WB - dati scaricati in precedenza)

--- R/bt_transfer.R
+++ R/bt_transfer.R
@@ -1,50 +1,51 @@
+#' Computing transfer factos for unit value trasnfer with income adjustment
+#'
+#' \code{bt_transfer} returns a data frame with all parameters used to calculate
+#' transfer factors which can be in turn used to perform value transfers in
+#' environmental and health economics.
+#'
+#' This function allows the calculation of transfer factors to be used to perform
+#' unit value transfers with income adjustments. Such factors are scalars that must be
+#' multiplied by the original value. The function allows the user to select the
+#' study site (country in which the original stusy that identified the value to be
+#' transferred was carried out) and the policy sites. The latter can be a group of
+#' countries or an aggregate of countries. Study and policy years can also be selected,
+#' as well as whether the original estimate is expressed in US dollars or euro (default).
+#' Finally, the function considers different values for income elasticity of the
+#' willingness to pay for the environmental/health good to be transferred (epsilon)
+#' according to the policy country's GNI per capita. More information on value
+#' transfer techniques can be found in Navrud and Ready (2007).
+#'
+#' @param study_site A single string. The name of the country in which the original
+#' study that identified the values to be transferred was carried out. The name can be
+#' provided in any language (first letter always capitalized). The default value is \code{EUU}
+#' which statnds for the European Union.
+#' @param policy_site A string vector. Names of the countries to which the value must be
+#' transferred. It can be either a single string or a vector. Names can be provided in
+#' any language.
+#' @param study_yr Numeric. The year in which the original values was estimated. Default
+#' is 2016, year about which the estimates provided by the European Commission's Handbook
+#' on the external cost of the transport sector refer to.
+#' @param policy_yr Numeric. The ywar to which values must be transferred. Default is 2019.
+#' @param aggregate A single string. It can assume three different values. If \code{no} transfer
+#' factors are provided for each country selected via \code{policy_site}. If \code{yes} a transfer
+#' factor is calculated for the aggregate made up by the countries selected via \code{policy_site}.
+#' If \code{row} a transfer factor is calculated for the aggregate "rest of the world" (world
+#' minus countries selected via \code{policy_site}).
+#' @param currency A single string. It can assume two values: \code{EUR} (the default) or
+#' \code{USD}. It refers to the currency in which the original values is expressed.
+#' @return A data frame containing all parameters used and the transfer factor (bt_fct), which
+#' is the scalar which the original value must be multiplied by to perform the transfer. Such
+#' factor is already adjusted for inflation.
+#' @examples
+#' bt_transfer(policy_site = c("Italia", "Allemagne", "France", "España", "Polska"))
+#' bt_transfer(study_site = "United States",
+#' policy_site = c("Italia", "Allemagne", "France", "España", "Polska"),
+#' currency = "USD")
+#' bt_transfer(policy_site = c("Italia", "Allemagne", "France", "España", "Polska"), aggregate = "yes")
+#' bt_transfer(policy_site = c("Italia", "Allemagne", "France", "España", "Polska"), aggregate = "row")
 bt_transfer <- function (study_site = "EUU", policy_site, study_yr = 2016, policy_yr = 2019,
                          aggregate = "no", currency = "EUR") {
-
-  # income levels and epsilon
-
-  income_lvs <- wbstats::wb_countries()[, c("iso3c", "income_level_iso3c", "income_level")]
-
-  income_lvs <- subset(income_lvs, !is.na(income_level_iso3c))
-
-  epsilon <- income_class[, c("income_level_iso3c", "epsilon")]
-
-  epsilon <- merge(income_lvs, epsilon)[, c("iso3c", "epsilon")]
-
-  epsilon$epsilon <- as.numeric(epsilon$epsilon)
-
-  # adding epsilon for the EUU
-
-  epsilon[nrow(epsilon) + 1,] = c("EUU", 0.2)
-
-  # Downloading GDP deflators
-
-  if (currency == "EUR") {
-
-    # downloading eurozone gdp deflator, for damage cost factors expressed in euro
-
-    gdp_defl <- eurostat::get_eurostat("nama_10_gdp", time_format = "num",
-                             filters = list(unit = "PD15_EUR", na_item = "B1GQ"))
-
-    gdp_defl_study <- subset(gdp_defl, geo == "EA" & time == study_yr)$values
-
-    # multiplying factor to be used to account for inflation from study to policy year
-    gdp_defl_fct <- subset(gdp_defl, geo == "EA" & time == policy_yr)$values / gdp_defl_study
-
-  } else if (currency == "USD") {
-
-    defl_exc <- wbstats::wb_data(c("NY.GDP.DEFL.ZS", "PA.NUS.FCRF"), country = c("EMU", "USA"),
-                     start_date = study_yr, end_date = policy_yr)
-
-    gdp_defl_study <- subset(defl_exc, date == study_yr & iso3c == "USA")$NY.GDP.DEFL.ZS
-
-    gdp_defl_fct <- subset(defl_exc, date == policy_yr & iso3c == "USA")$NY.GDP.DEFL.ZS / gdp_defl_study
-
-    # USD-EUR policy year exchange rate. USD per EUR (values in dollars ,ust be multiplied for this factor)
-
-    us_euro <- subset(defl_exc, date == policy_yr & iso3c == "EMU")$PA.NUS.FCRF
-
-    }
 
   # identifying iso3c codes of provided study and policy sites
 
@@ -62,30 +63,57 @@ bt_transfer <- function (study_site = "EUU", policy_site, study_yr = 2016, polic
 
   }
 
+  # income levels and epsilon
+
+  epsilon <- income_class[, c("income_level_iso3c", "epsilon")]
+
+  epsilon <- merge(income_lvs, epsilon)[, c("iso3c", "epsilon")]
+
+  epsilon$epsilon <- as.numeric(epsilon$epsilon)
+
+  # adding epsilon for the EUU
+
+  epsilon[nrow(epsilon) + 1,] = c("EUU", 0.2)
+
+  # Defining gdp per capita data of the study site
+
+  study_site_gdp <- subset(wb_series, iso3c == iso_study & year == study_yr)$gdp_capita
+
+  # Defining GDP deflator to be used
+
+  if (currency == "EUR") {
+
+    gdp_defl_study <- subset(gdp_defl_eurostat, eu_code == "EA" & year == study_yr)$gdp_defl
+
+    # multiplying factor to be used to account for inflation from study to policy year
+    gdp_defl_fct <- subset(gdp_defl_eurostat, eu_code == "EA" & year == policy_yr)$gdp_defl / gdp_defl_study
+
+  } else if (currency == "USD") {
+
+    gdp_defl_study <- subset(wb_series, iso3c == "USA" & year == study_yr)$gdp_defl
+
+    gdp_defl_fct <- subset(wb_series, iso3c == "USA" & year == policy_yr)$gdp_defl / gdp_defl_study
+
+    # USD-EUR policy year exchange rate. USD per EUR (values in dollars ,ust be multiplied for this factor)
+
+    us_euro <- subset(wb_series, iso3c == "EMU" & year == policy_yr)$exc_rate
+
+    }
+
   if (aggregate == "no") {
 
-    # donwloading gdp per capita data (PPP) and GNI per capita (Atlas method)
-
-    wb_series <- wbstats::wb_data(c("NY.GDP.PCAP.PP.KD"),
-                                  country = c(iso_study, iso_policy),
-                     start_date = study_yr, end_date = policy_yr)
-
-    wb_series <- wb_series[, c("iso3c", "date", "NY.GDP.PCAP.PP.KD")]
-
+    gdp_capita <- subset(wb_series, iso3c %in% iso_policy & year == policy_yr,
+                         select = c(iso3c, year, gdp_capita))
 
     # assigning epsilon according to income levels and computing transfer factor (adjusted for inflation)
 
-    bt_fct <- merge(wb_series, epsilon, all.x = TRUE)
-
-    study_site_gdp <- subset(bt_fct, date == study_yr & iso3c == study_site)$NY.GDP.PCAP.PP.KD
-
-    bt_fct <- subset(bt_fct, date == policy_yr)
+    bt_fct <- merge(gdp_capita, epsilon, all.x = TRUE)
 
     bt_fct$epsilon <- as.numeric(bt_fct$epsilon)
 
     bt_fct <- within(bt_fct, { # value transfer factors adjusted for inflation
 
-      bt_fct <- ((NY.GDP.PCAP.PP.KD / study_site_gdp)^epsilon) * gdp_defl_fct
+    bt_fct <- ((gdp_capita / study_site_gdp)^epsilon) * gdp_defl_fct
 
     })
 
@@ -95,7 +123,7 @@ bt_transfer <- function (study_site = "EUU", policy_site, study_yr = 2016, polic
 
  } else if (currency == "USD") {
 
-   bt_fct$bt_fct <- bt_fct$bt_fct* us_euro
+   bt_fct$bt_fct <- bt_fct$bt_fct * us_euro
 
    bt_fct
  }
@@ -104,54 +132,77 @@ bt_transfer <- function (study_site = "EUU", policy_site, study_yr = 2016, polic
   } else if (aggregate == "yes") {
 
     # downloading gdp (PPP) and population data to compute aggregate's gdp per capita.
-    # donwloading also GNI per capita (current USD) to identify appropriate epsilon
+    # donwloading also GNI (current USD) to identify appropriate epsilon
 
-    # downloading gdp per capita data of the study site
+    gdp_pop <- subset(wb_series,
+                      iso3c %in% iso_policy & year == policy_yr,
+                      select = c(iso3c, year, gdp, gni, pop))
 
-    study_site_gdp <- wbstats::wb_data(c("NY.GDP.PCAP.PP.KD"),
-                     country = iso_study,
-                     start_date = study_yr, end_date = policy_yr)
-
-    study_site_gdp <- subset(study_site_gdp, date == study_yr & iso3c == study_site)$NY.GDP.PCAP.PP.KD
-
-    wb_series <- wbstats::wb_data(c("NY.GDP.MKTP.PP.KD", "SP.POP.TOTL"),
-                                  country = iso_policy,
-                                  start_date = study_yr, end_date = policy_yr)
-
-    gdp_pop <- wb_series[, c("iso3c", "date", "NY.GDP.MKTP.PP.KD",
-                               "SP.POP.TOTL")]
-
-    bt_fct <- merge(gdp_pop, epsilon, all.x = TRUE)
-
-    bt_fct$epsilon <- as.numeric(bt_fct$epsilon)
-
-    bt_fct_l <- split(bt_fct, bt_fct$date)
-
-    bt_fct_l <- lapply(bt_fct_l, function (x) {
-
-      data.frame(date = unique(x$date),
-                 NY.GDP.MKTP.PP.KD = sum(x$NY.GDP.MKTP.PP.KD),
-                 SP.POP.TOTL = sum(x$SP.POP.TOTL),
-                 epsilon = weighted.mean(x$epsilon, x$SP.POP.TOTL))
-    })
-
-    bt_fct <- do.call("rbind", bt_fct_l)
-
-    bt_fct <- within(bt_fct, {
-
-      gdp_capita <- NY.GDP.MKTP.PP.KD / SP.POP.TOTL
-      NY.GDP.MKTP.PP.KD <- NULL
-      SP.POP.TOTL <- NULL
-
-    })
+    bt_fct <- data.frame(gdp_capita = sum(gdp_pop$gdp) / sum(gdp_pop$pop),
+               gni_capita = sum(gdp_pop$gni) / sum(gdp_pop$pop))
 
     # calculating trasnfer factors
 
-    bt_fct <- subset(bt_fct, date == policy_yr)
+    # identifying epsilon on the basis of the latest income classification by the WB
+
+    epsilon_agg <- income_class
+
+    epsilon_agg$gni_agg <- ifelse(bt_fct$gni_capita < income_class$max & bt_fct$gni_capita >= income_class$min,
+                                  TRUE, FALSE)
+
+    bt_fct$epsilon <- subset(epsilon_agg, gni_agg == TRUE)$epsilon
 
     bt_fct <- within(bt_fct, { # value transfer factors adjusted for inflation
 
-      bt_fct <- ((gdp_capita / study_site_gdp)^epsilon) * gdp_defl_fct
+    bt_fct <- ((gdp_capita / study_site_gdp)^epsilon) * gdp_defl_fct
+
+    })
+
+    rownames(bt_fct) <- NULL
+
+    if (currency == "EUR") {
+
+      bt_fct
+
+    } else if (currency == "USD") {
+
+      bt_fct$bt_fct <- bt_fct$bt_fct* us_euro
+
+      bt_fct
+    }
+
+  }else if (aggregate == "row"){
+
+    gdp_pop_wld <- subset(wb_series, iso3c == "WLD" & year == policy_yr)
+
+    gdp_pop <-  subset(wb_series, iso3c %in% iso_policy & year == policy_yr)
+
+    gdp_pop <- data.frame(gdp = sum(gdp_pop$gdp),
+               gni = sum(gdp_pop$gni),
+               pop = sum(gdp_pop$pop))
+
+    gdp_pop_row <- within(gdp_pop, {
+
+      gdp_capita <- (gdp_pop_wld$gdp - gdp) / (gdp_pop_wld$pop - pop)
+      gni_capita <- (gdp_pop_wld$gni - gni) / (gdp_pop_wld$pop - pop)
+      gdp <- NULL
+      gni <- NULL
+      pop <- NULL
+
+    })
+
+    # identifying epsilon on the basis of GNI per capita (Atlas method)
+
+    epsilon_row <- income_class
+
+    epsilon_row$gni_row <- ifelse(gdp_pop_row$gni_capita < income_class$max & gdp_pop_row$gni_capita >= income_class$min,
+                                   TRUE, FALSE)
+
+    gdp_pop_row$epsilon <- subset(epsilon_row, gni_row == TRUE)$epsilon
+
+    bt_fct <- within(gdp_pop_row, {
+
+      bt_fct <- (gdp_capita / study_site_gdp)^epsilon * gdp_defl_fct
 
     })
 
@@ -166,7 +217,8 @@ bt_transfer <- function (study_site = "EUU", policy_site, study_yr = 2016, polic
       bt_fct
     }
 
-  }else if (aggregate == "row")
+
+  }
 
 
 }
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -3,4 +3,31 @@
 # loading income classification
 income_class <- read.csv(here::here("data-raw/income_class.csv"), sep = ";")
 
-usethis::use_data(income_class, overwrite = TRUE)
+# downloading data from the world bank database
+
+wb_series <- wbstats::wb_data(c("NY.GDP.PCAP.PP.KD", "NY.GDP.MKTP.PP.KD",
+                   "SP.POP.TOTL", "NY.GNP.ATLS.CD", "NY.GDP.DEFL.ZS", "PA.NUS.FCRF"),
+                   country = c("all", "WLD", "EUU"))
+
+names(wb_series) <- c("iso2c", "iso3c", "country", "year", "gdp_defl", "gdp", "gdp_capita",
+                      "gni", "exc_rate", "pop")
+
+wb_series <- wb_series[, -1]
+
+# downloading Euro Area gdp deflator
+
+gdp_defl_eurostat <- eurostat::get_eurostat("nama_10_gdp", time_format = "num",
+                                   filters = list(unit = "PD15_EUR", na_item = "B1GQ"))
+
+gdp_defl_eurostat <- gdp_defl_eurostat[, -c(1,2)]
+
+names(gdp_defl_eurostat) <- c("eu_code", "year", "gdp_defl")
+
+# Downloading WB income classification
+
+income_lvs <- wbstats::wb_countries()[, c("iso3c", "income_level_iso3c", "income_level")]
+
+income_lvs <- subset(income_lvs, !is.na(income_level_iso3c))
+
+usethis::use_data(income_class, wb_series, gdp_defl_eurostat, income_lvs, overwrite = TRUE)
+
